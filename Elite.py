13cfd0a34a7359072ffdfd6e94db10efabfec2ab
ratiswetabhagat0412-byte <ratiswetabhagat0412@gmail.com>
2026-09-26 11:50:30 +0530

    Change AI model to gemini-2.5-flash-lite
    Updated model to 'gemini-2.5-flash-lite' for content generation.

--- Elite.py
+++ Elite.py
@@ -133,8 +133,8 @@ if user_prompt_display and user_parts:
             payload_contents = st.session_state.gemini_history + [current_user_content]
 
             # Model updated to gemini-3.8-flash
-            response = client.models.generate_content_stream(
-                model="gemini-3.8-flash",
+           response = client.models.generate_content_stream(
+                model="gemini-2.5-flash-lite",   # <--- ✅ YAHAN YE DAALO
                 contents=payload_contents,
                 config=types.GenerateContentConfig(
                     system_instruction=system_prompt,
