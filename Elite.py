8805444070140298afce3b9c5cd431651210adc9
ratiswetabhagat0412-byte <ratiswetabhagat0412@gmail.com>
2026-09-26 12:06:21 +0530

    Refactor Elite.py to improve functionality

--- Elite.py
+++ Elite.py
@@ -136,10 +136,10 @@ if user_prompt_display and user_parts:
             payload_contents = st.session_state.gemini_history + [current_user_content]
 
             response = client.models.generate_content_stream(
-                model="gemini-2.5-flash",
+                model="gemini-3.8-flash",
                 contents=payload_contents,
                 config=types.GenerateContentConfig(
-                    system_instruction=system_prompt,
+                    system_instruction=system_prompt
                 )
             )
 
